232cf5d3d64576a73e36b5d6226d63acbbaf02f3
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-08 10:41:02 -0400

    created tables in the database

---
+++ commands.sql
@@ -0,0 +1,39 @@
+CREATE Table students(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    hometown VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+CREATE Table admins(
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+CREATE Table interests (
+    id INT PRIMARY KEY,
+    title VARCHAR(100)
+);
+
+CREATE Table groups (
+    id INT PRIMARY KEY
+);
+
+CREATE Table students_to_interest (
+    id INT PRIMARY KEY,
+    student_id INT,
+    interest_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (interest_id) REFERENCES interests(id)
+);
+
+CREATE Table students_to_groups (
+    id INT PRIMARY KEY,
+    student_id INT,
+    group_id INT,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+);
