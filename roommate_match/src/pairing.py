ffa3ace6665569fad78ba7ab4cec457a1a487405
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-10 14:50:09 -0400

    Added implementations for pairing class

--- roommate_match/src/pairing.py
+++ roommate_match/src/pairing.py
@@ -6,7 +6,13 @@ class pairing:
         self.user2_accepted = user2_accepted
 
     def get_user1_id(self):
-        return None
+        return self.user1_id
 
     def get_user2_id(self):
-        return None
+        return self.user2_id
+    
+    def get_user3_id(self):
+        return self.user2_id
+    
+    def get_user4_id(self):
+        return self.user2_id
