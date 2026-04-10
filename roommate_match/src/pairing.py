3e14324da0b6b2853646eabebe69c2c4b86fe442
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-10 14:47:03 -0400

    Added pairing class file without implementation

---
+++ roommate_match/src/pairing.py
@@ -0,0 +1,10 @@
+class pairing:
+    def __init__(self, user1_id, user2_id):
+        self.user1_id = user1_id
+        self.user2_id = user2_id
+
+    def get_user1_id(self):
+        return None
+
+    def get_user2_id(self):
+        return None
