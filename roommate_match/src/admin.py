42a99c718c3e1ff95fc3085a00fa0ecece49aab3
juliadicostanzo <jdicostanzo@ithaca.edu>
2026-04-10 13:45:58 -0400

    added __init__.py and failing tests for admin

---
+++ roommate_match/src/admin.py
@@ -0,0 +1,24 @@
+
+
+class Admin:
+    def __init__(self):
+        self.students = []
+        self.matches = []
+    
+    def generateId(self):
+        pass
+
+    def addStudent(self, name, email, password, hometown):
+        pass
+
+    def removeStudent(self, id):
+        pass
+
+    def viewAllStudents(self):
+        pass
+
+    def getStudentByName(self, name):
+        pass
+
+    def getStudentById(self, id):
+        pass
