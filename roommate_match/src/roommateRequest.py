f3c9ca1b032bb0a3a784b7834d873bc194e56c99
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-10 10:24:24 -0400

    Added failing tests for roommateRequest Changed constructor from status to accepted

--- roommate_match/src/roommateRequest.py
+++ roommate_match/src/roommateRequest.py
@@ -1,12 +1,12 @@
 class roommateRequest:
    
-    def __init__(self, sender_id, receiver_id, status):
+    def __init__(self, sender_id, receiver_id):
         self.sender_id = sender_id
         self.receiver_id = receiver_id
-        self.status = status
+        self.accepted = None
 
     def accept_request(self):
-        self.status = "accepted"
+        self.accepted = True
 
     def reject_request(self):
-        self.status = "rejected"
+        self.accepted = False
---
+++ roommate_match/test/roommateRequestTest.py
@@ -0,0 +1,13 @@
+import unittest
+import roommateRequest
+
+class TestRoommateRequest(unittest.TestCase):
+
+    def setUp(self):
+        self.request = roommateRequest.roommateRequest(1, 2)
+
+    def test_accept_request(self):
+        return IllegalStateException("Not implemented yet")
+
+    def test_reject_request(self):
+        return IllegalStateException("Not implemented yet")
