8afb9b85b004164995e763da41b8c4609511da36
Liam Nelsen <lnelsen@ithaca.edu>
2026-04-13 10:36:21 -0400

    added empty tests for the methods I have not implemented yet, and added more tests to the old ones

--- roommate_match/test/student_test.py
+++ roommate_match/test/student_test.py
@@ -5,26 +5,63 @@ class StudentTest(unittest.TestCase):
     student1 = Student(123, "John Doe", "john.doe@example.com", "password123", "New York")
     student2 = Student(234, "Jane Smith", "jane.smith@example.com", "password456", "Los Angeles")
 
+    def test_sendRequest(self):
+        pass
+
+    def test_respondRequest(self):
+        pass
+    
     def test_updatePassword(self):
         self.student1.updatePassword("newpassword123")
         self.assertEqual(self.student1.password, "newpassword123")
 
+        self.student2.updatePassword("newpassword456")
+        self.assertEqual(self.student2.password, "newpassword456")
+
         with self.assertRaises(ValueError):
             self.student1.updatePassword("")
 
+        with self.assertRaises(ValueError):
+            self.student2.updatePassword("")
+
     def test_updateName(self):
-        self.student1.updateName("Johnathan Doe")
-        self.assertEqual(self.student1.name, "Johnathan Doe")
+        self.student1.updateName("John Doe")
+        self.assertEqual(self.student1.name, "John Doe")
+
+        self.student2.updateName("Juliet Smith")
+        self.assertEqual(self.student2.name, "Juliet Smith")
 
         with self.assertRaises(ValueError):
             self.student1.updateName("")
+
+        with self.assertRaises(ValueError):
+            self.student2.updateName("")
 
     def test_updateHometown(self):
         self.student1.updateHometown("Chicago")
         self.assertEqual(self.student1.hometown, "Chicago")
 
+        self.student2.updateHometown("Nashville")
+        self.assertEqual(self.student2.hometown, "Nashville")
+
+
         self.student1.updateHometown("")
         self.assertEqual(self.student1.hometown, "")
+
+        self.student2.updateHometown("")
+        self.assertEqual(self.student2.hometown, "")
+
+    def test_updateInterests(self):
+        pass
+
+    def test_updatePreferences(self):
+        pass
+
+    def test_viewStudents(Self):
+        pass
+
+    def test_searchStudents(Self):
+        pass
 
 
 if __name__ == '__main__':
