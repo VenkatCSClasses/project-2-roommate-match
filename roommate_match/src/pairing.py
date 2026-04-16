45f9fbf44d2da758360e25f3d05ce4cdc4d07737
baneetpukhrambam <bpukhrambam@ithaca.edu>
2026-04-16 10:25:49 -0400

    Added group_id to pairing class

--- roommate_match/src/pairing.py
+++ roommate_match/src/pairing.py
@@ -1,9 +1,13 @@
 class pairing:
-    def __init__(self, user1_id, user2_id, user3_id=None, user4_id=None):
+    def __init__(self, group_id, user1_id, user2_id, user3_id=None, user4_id=None):
+        self.group_id = group_id
         self.user1_id = user1_id
         self.user2_id = user2_id
         self.user3_id = user3_id
         self.user4_id = user4_id
+
+    def get_group_id(self):
+        return self.group_id
 
     def get_user1_id(self):
         return self.user1_id
